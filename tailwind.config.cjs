575ee2524324f0663c05ea87214897c3f2acd190
gabrieldemian <gabrielgcr45@gmail.com>
2023-01-18 22:52:35 -0300

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,astro}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx,astro}'],
   theme: {
     container: {
-      center: true
+      center: true,
     },
     extend: {
       fontFamily: {
         poppins: 'poppins',
-        corben: 'corben'
-      }
-    }
+        corben: 'corben',
+      },
+    },
   },
   plugins: [
     require('@catppuccin/tailwindcss')({
-      defaultFlavour: 'mocha'
-    })
-  ]
+      defaultFlavour: 'mocha',
+    }),
+  ],
 }
