cb6156af7024cdbe687be0ece43c9ed1c2caf69f
gabrieldemian <gabrielgcr45@gmail.com>
2023-01-23 23:00:13 -0300

    added portfolio section

--- src/components/ui/Project/Project.tsx
+++ src/components/ui/Project/Project.tsx
@@ -7,9 +7,19 @@ interface Props {
 const Project = (props: Props) => {
   return (
     <div>
-      <p>{props.title}</p>
+      <a
+        class="cursor-pointer overflow-none"
+        href={`/portfolio/${props.title.toLowerCase().replace(' ', '-')}`}
+      >
+        <img
+          class="rounded-lg hover:scale-[105%] duration-300 aspect-video object-cover"
+          width="900px"
+          height="900px"
+          src={props.img}
+        />
+      </a>
+      <h6 class="font-bold text-white">{props.title}</h6>
       <p>{props.desc}</p>
-      <p>{props.img}</p>
     </div>
   )
 }
