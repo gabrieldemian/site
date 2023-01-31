661e5e1e50516278982b3f17646ea5fd103321b6
gabrieldemian <gabrielgcr45@gmail.com>
2023-01-30 22:48:08 -0300

    refactor portfolio items

--- src/components/ui/Project/Project.tsx
+++ src/components/ui/Project/Project.tsx
@@ -2,27 +2,35 @@ interface Props {
   title: string
   description: string
   image: string
+  class?: string
 }
 
-const Project = (props: Props) => {
+const Project = ({
+  title,
+  description,
+  image,
+  class: className = '',
+}: Props) => {
   return (
-    <div>
-      <a
-        aria-label={`Click on the image to read more about ${props.title}`}
-        class="cursor-pointer glow"
-        href={`/portfolio/${props.title.toLowerCase().replace(' ', '-')}`}
-      >
-        <img
-          class="rounded-lg aspect-video object-cover"
-          width="900px"
-          height="900px"
-          alt={'image of '.concat(props.title)}
-          src={props.image}
-        />
-      </a>
-      <h3 class="font-bold text-white mt-3">{props.title}</h3>
-      <p>{props.description}</p>
-    </div>
+    <a
+      aria-label={`Click on the image to read more about ${title}`}
+      class={`cursor-pointer glow relative [&_footer]:hover:opacity-100 ${className}`}
+      href={`/portfolio/${title.toLowerCase().replace(' ', '-')}`}
+    >
+      <img
+        class="rounded-lg aspect-video object-cover"
+        width="900px"
+        height="900px"
+        alt={'image of '.concat(title)}
+        src={image}
+      />
+      <footer class="absolute flex inset-0 bg-crust/50 p-5 opacity-0 duration-300 delay-150 ease-linear">
+        <div class="mt-auto">
+          <h3 class="font-bold text-white mt-3">{title}</h3>
+          <p class="text-text">{description}</p>
+        </div>
+      </footer>
+    </a>
   )
 }
 
